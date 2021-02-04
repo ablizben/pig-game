f9bf70755b1138e20c068ff8645b1afd3e586adf
Abby Benjamin <ablizben@gmail.com>
2021-02-04 13:36:29 -0500

    adding new game and instructions btn/modal window

--- script.js
+++ script.js
@@ -12,16 +12,38 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const btnOpenInstruct = document.querySelector('.btn--instruct');
+const btnCloseInstruct = document.querySelector('.close-modal');
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+
+
+let scores, currentScore, activePlayer, playing;
 
 
 //starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+
+//beginning of game
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -31,33 +53,60 @@ const switchPlayer = function () {
     player1El.classList.toggle('player--active');
 };
 
-//rolling dice
+//rolling dice functionality
 btnRoll.addEventListener('click', function () {
-    //1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    //2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
-    //3. Check for rolled 1: if true, switch to the next player
-    if (dice !== 1) {
-        //add the dice number to the current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
-        //switch to next player
-        switchPlayer();
+    if (playing) {
+        //1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        //2. Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+        //3. Check for rolled 1: if true, switch to the next player
+        if (dice !== 1) {
+            //add the dice number to the current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            //switch to next player
+            switchPlayer();
+        }
     }
 });
 
 
+//hold btn functionality
 btnHold.addEventListener('click', function () {
-    //1. add current score to the score of active player
-    scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-    //2. check score is already at least 50
+    if (playing) {
+        //1. add current score to the score of active player
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+        //2. check score is already at least 50
+        if (scores[activePlayer] >= 20) {
+            //finish game
+            playing = false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        } else {
+            //switch to next player
+            switchPlayer();
+        }
+    }
+});
 
-    //finish game
+//refresh game at any time
+btnNew.addEventListener('click', init);
 
-    //switch to next player
-    switchPlayer();
+//open game instructions
+btnOpenInstruct.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+})
+
+//close game instructions
+btnCloseInstruct.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+
 })
